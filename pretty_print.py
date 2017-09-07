102d4c2d71d80928bc415451f698ed860ea52a31
Umair Sarfraz <aquadestructor@icloud.com>
2017-09-08 03:58:31 +0500

    Pretty print everything

--- pretty_print.py
+++ pretty_print.py
@@ -3,18 +3,15 @@ from colorama import init, deinit, Fore, Style
 colors = dict(
     yellow=Fore.YELLOW,
     blue=Fore.BLUE,
-    green=Fore.GREEN
+    green=Fore.GREEN,
+    red=Fore.RED
 )
 
 
 class PrettyPrint:
     def __init__(self, text, *args, **kwargs):
-        init()  # Initialize colorama
+        init(autoreset=True)  # Initialize colorama
         self.print_text(text, *args, **kwargs)
 
-    def print_text(self, text, color=Fore.BLUE, style=Style.BRIGHT):
+    def print_text(self, text, color=Fore.YELLOW, style=Style.BRIGHT):
         print(color + style + text)
-
-    def __del__(self):
-        deinit()
-
