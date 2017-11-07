5a660ae3dc82913b8ff430dbdd28f5c0a9e65818
bahamapascal <bahamapascal@yahoo.com>
2017-11-07 13:01:37 +0100

    Fix get_inputs()

--- Start CL Wallet.bat
+++ Start CL Wallet.bat
@@ -1,5 +1,5 @@
 @echo off
 if not "%1" == "max" start /MAX cmd /c %0 max & exit/b
 mode con: cols=140 lines=2000
-python Wallet.py
+C:\Users\Pascal\AppData\Local\Programs\Python\Python36\python Wallet.py
 pause
